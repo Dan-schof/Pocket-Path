f102f1d3018873594c0e19868436f6767652961e
Dan-schof <danieldgschofield@gmail.com>
2026-03-07 15:29:11 +0000

    Make PocketPath PythonAnywhere Legible

--- main.py
+++ main.py
@@ -72,4 +72,4 @@ def home():
     return render_template_string(HTML_TEMPLATE, rem=rem, runway=runway, total=total)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run()
